29df727d92b3befef74e35bef8e905f2a226d20d
Davi S. Nascimento <100232646+DaviSNascimento@users.noreply.github.com>
2023-03-17 00:21:23 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+    System.out.println("Escreva Seu nome:");
+    Scanner nome = new Scanner(System.in);
+    System.out.println("Olá " + nome + ". Seja bem vindo!");
+  
+  
+  }
+}
